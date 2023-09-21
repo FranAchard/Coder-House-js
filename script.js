f04c21e95c499c5ed2800232d17f54d0a55fa44e
FranAchard <franachard8@gmail.com>
2023-09-21 00:17:21 -0300

    Juego falta estilos

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Definimos un array con las cartas del juego
 const cartas = [
-    "A", "A", "B", "B", "C", "C", "D", "D",
-    "E", "E", "F", "F", "G", "G", "H", "H"
+    "./Img/Trunks.png", "./Img/gohan.png", "./Img/goku.png", "./Img/vegeta.png",
+    "./Img/vegeta.png", "./Img/vegito.png", "./Img/gohan.png", "./Img/vegito.png",
+    "./Img/goku.png", "./Img/picoro.png", "./Img/gogeta.png", "./Img/broly.png",
+    "./Img/gogeta.png", "./Img/broly.png", "./Img/picoro.png", "./Img/Trunks.png"
 ];
 
 const maxMovimientos = 15; // Máximo número de movimientos permitidos
@@ -10,7 +11,7 @@ let cartasDestapadas = [];
 let aciertos = 0;
 let movimientos = 0;
 let intentos = 0;
-let tiempoRestante = 45; // Tiempo en segundos
+let tiempoRestante = 30; // Tiempo en segundos
 let timerInterval;
 let juegoIniciado = false; // Variable para controlar si el juego ha comenzado
 
@@ -22,45 +23,42 @@ function barajarCartas() {
 // Función para destapar una carta
 function destapar(id) {
     if (!juegoIniciado || cartasDestapadas.length >= 2 || intentos >= maxMovimientos) {
-        return; // No hacer nada si el juego no ha comenzado o se alcanzó el máximo de movimientos
+        return; 
     }
 
-    const boton = document.getElementById(id.toString());
+    const imagen = document.getElementById(id.toString());
 
     // Verificar si la carta ya está destapada
-    if (boton.textContent !== "?") {
-        return;
-    }
+    if (cartasDestapadas.length < 2 && imagen.getAttribute("data-destapada") !== "true") {
+        imagen.src = cartas[id];
+        imagen.setAttribute("data-destapada", "true");
+        cartasDestapadas.push({ id, imagen });
 
-    const carta = cartas[id];
-    boton.textContent = carta;
-    cartasDestapadas.push({ id, carta });
+        if (cartasDestapadas.length === 2) {
+            movimientos++;
+            document.getElementById("movimientos").textContent = `Movimientos: ${movimientos}`;
 
-    if (cartasDestapadas.length === 2) {
-        movimientos++;
-        document.getElementById("movimientos").textContent = `Movimientos: ${movimientos}`;
+            if (cartasDestapadas[0].imagen.src === cartasDestapadas[1].imagen.src) {
+                aciertos++;
+                document.getElementById("aciertos").textContent = `Aciertos: ${aciertos}`;
+                cartasDestapadas = [];
 
-        if (cartasDestapadas[0].carta === cartasDestapadas[1].carta) {
-            // Si las cartas son iguales, incrementa la variable aciertos
-            aciertos++;
-            document.getElementById("aciertos").textContent = `Aciertos: ${aciertos}`;
-            cartasDestapadas = [];
-
-            if (aciertos === cartas.length / 2) {
-                clearInterval(timerInterval);
-                alert("¡Has ganado!");
+                if (aciertos === cartas.length / 2) {
+                    clearInterval(timerInterval);
+                    alert("¡Has ganado!");
+                }
+            } else {
+                setTimeout(() => {
+                    ocultarCartas();
+                }, 1000);
             }
-        } else {
-            setTimeout(() => {
-                ocultarCartas();
-            }, 1000);
-        }
 
-        intentos++;
+            intentos++;
 
-        if (intentos >= maxMovimientos) {
-            // Verificar si el usuario ha agotado los movimientos disponibles
-            alert("Te quedaste sin movimientos");
+            if (intentos >= maxMovimientos) {
+                // Verificar si el usuario ha agotado los movimientos disponibles
+                alert("Te quedaste sin movimientos");
+            }
         }
     }
 }
@@ -68,8 +66,8 @@ function destapar(id) {
 // Función para ocultar las cartas destapadas
 function ocultarCartas() {
     for (const carta of cartasDestapadas) {
-        const boton = document.getElementById(carta.id.toString());
-        boton.textContent = "?";
+        carta.imagen.src = "./Img/blank.png";
+        carta.imagen.removeAttribute("data-destapada");
     }
     cartasDestapadas = [];
 }
@@ -94,26 +92,24 @@ function iniciarJuego() {
         aciertos = 0;
         movimientos = 0;
         intentos = 0;
-        juegoIniciado = true; // Marcamos que el juego ha comenzado
+        juegoIniciado = true;
 
         // Ocultar el botón "Start" después de iniciado el juego
         document.getElementById("startButton").style.display = "none";
 
-        // Establecer el contenido de los elementos HTML aquí
         document.getElementById("movimientos").textContent = "Movimientos: 0";
         document.getElementById("aciertos").textContent = "Aciertos: 0";
         document.getElementById("t-restante").textContent = `Tiempo restante: ${tiempoRestante} Segundos`;
 
         iniciarTemporizador();
 
-        const botones = document.querySelectorAll("button");
-        for (let i = 0; i < botones.length; i++) {
-            botones[i].addEventListener("click", () => destapar(i));
+        const imagenes = document.querySelectorAll(".cartas");
+        for (let i = 0; i < imagenes.length; i++) {
+            imagenes[i].addEventListener("click", () => destapar(i));
         }
     }
 }
 
-// Inicializa el juego al cargar la página
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     iniciarJuego();
 });
